180231e5ae59c0577af62eb89821195ae7e30139
Adrian <adrianbravo145@gmail.com>
2021-09-19 21:12:27 -0300

    loading search

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -5,6 +5,7 @@ import { switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 import { Result, MovieResponse } from '../models/movie.interface';
 
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -16,11 +17,13 @@ export class SearchComponent implements OnInit {
   public conteo: any = 9;
   public page = 1;
   public textoSearched!: string;
+  public loading!: boolean;
 
   constructor(private activatedRoute: ActivatedRoute,
               private router: Router, private movieService: MoviesService) { }
 
   ngOnInit(): void {
+    this.loading = true;
     this.activatedRoute.params.subscribe(({termino}) => {
       this.buscarPelicula(termino, String(this.page));
     });
@@ -29,9 +32,14 @@ export class SearchComponent implements OnInit {
   async buscarPelicula(texto: string, page = '1'): Promise<void> {
    const text = texto.trim();
    this.textoSearched = text;
-   const resp = await this.movieService.searchMovie(text, page)?.toPromise();
-   resp?.results?.map((result: any) => result.visible = false);
-   this.peliSerched = resp?.results?.slice(0, this.conteo);
+   this.loading = true;
+   setTimeout(async () => {
+     const resp = await this.movieService.searchMovie(text, page)?.toPromise();
+     resp?.results?.map((result: any) => result.visible = false);
+     this.peliSerched = resp?.results?.slice(0, this.conteo);
+     this.loading = false;
+     console.log({peliSerched: this.peliSerched});
+   }, 2000);
   }
 
   previous(numb: number): void{
